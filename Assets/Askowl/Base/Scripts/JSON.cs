712db5d98ad1b45bb3c46da12fb60b25eac82b21
paulmarrington <paul@marrington.net>
2018-06-13 14:03:12 +1000

    13/06/2018

--- Assets/Askowl/Base/Scripts/JSON.cs
+++ Assets/Askowl/Base/Scripts/JSON.cs
@@ -14,6 +14,11 @@ namespace Askowl {
     /// <param name="json">String with hopefully correctly formatted JSON</param>
     public Json(string json = null) { Parse(json); }
 
+    /// <summary>
+    /// Change the JSON to process on an existing Json instance. Recycling is good.
+    /// </summary>
+    /// <param name="jsonText">String with hopefully correctly formatted JSON</param>
+    /// <returns>false on error and sets <see cref="ErrorMessage"/></returns>
     public bool Parse(string jsonText) {
       json         = jsonText;
       idx          = 0;
@@ -25,18 +30,49 @@ namespace Askowl {
       return ErrorMessage != null;
     }
 
+    /// <summary>
+    /// Parsing the JSON creates a tree of nodes, arrays and leaf objects. You should not need to get to a Node, but it is here for if I a wrong.
+    /// </summary>
     public class Node : Dictionary<string, object> { };
 
+    /// <summary>
+    /// Returns the node at the location we have walked to in the tree - if it is of the type expected.
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <typeparam name="T">The type we anticipate this node to be</typeparam>
+    /// <returns>The requested node in the correct form for (T) or default&lt;T> if it can't be accessed</returns>
     public T Here<T>() { return CheckIsA<T>() ? (T) here : default(T); }
 
-    public bool IsA<T>() { return here is T; }
+    /// <summary>
+    /// Checks to see if we can cast the node type to that provided.
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <typeparam name="T">The type we anticipate this node to be</typeparam>
+    public bool IsA<T>() { return CheckIsA<T>(); }
 
+    /// <summary>
+    /// See if we have walked to a tree node, being a list of nodes accessed by name
+    /// </summary>
     public bool IsNode { get { return here is Node; } }
 
+    /// <summary>
+    /// See if we have walked to an Array node, being a list of nodes accessed by index
+    /// </summary>
     public bool IsArray { get { return here is Array; } }
 
+    /// <summary>
+    /// When completely lost, ask for the node type. It could be Node, Array, object, double, long, bool or null
+    /// </summary>
+    public Type NodeType { get { return here.GetType(); } }
+
+    /// <summary>
+    /// If we fail to parse the json, or later fail to retrieve a node by name, this will be set. When there are no errors it will be null.
+    /// </summary>
     public string ErrorMessage { get; private set; }
 
+    /// <summary>
+    /// Retrieve a count of the number of child nodes from our location. Will return one for nodes that are not tree nodes or arrays.
+    /// </summary>
     public int Count {
       get {
         return (IsNode)  ? ((Node) here).Count :
@@ -45,21 +81,68 @@ namespace Askowl {
     }
 
     #region AccessNodes
+    /// <summary>
+    /// Use Get if you know exactly what you are looking for.
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <param name="path">
+    /// Path to the target node either as a list of objects or a single string with nodes separated by '.'.
+    /// Array indexes can be numbers or strings that convert to numbers.
+    /// <example>"root", "level1", 0, "level 3"</example>
+    /// <example>"root.level1.0.level 3"</example>
+    /// </param>
+    /// <typeparam name="T">The type we anticipate this node to be</typeparam>
+    /// <returns>what you asked for or default&lt;T> if not found</returns>
     public T Get<T>(params object[] path) {
       Walk(path);
       return Here<T>();
     }
 
+    /// <summary>
+    /// Use to take a stroll down JSON lane - from the root/start.
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <param name="path">
+    /// Path to the target node either as a list of objects or a single string with nodes separated by '.'.
+    /// Array indexes can be numbers or strings that convert to numbers.
+    /// <example>"root", "level1", 0, "level 3"</example>
+    /// <example>"root.level1.0.level 3"</example>
+    /// </param>
+    /// <returns>false if path does not exist</returns>
     public bool Walk(params object[] path) {
-      here = root;
+      here         = root;
+      ErrorMessage = null;
       return WalkOn(path);
     }
 
+    /// <summary>
+    /// Shortcut to walk to a node and check the node is of the type expected.
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <param name="path">
+    /// Path to the target node either as a list of objects or a single string with nodes separated by '.'.
+    /// Array indexes can be numbers or strings that convert to numbers.
+    /// <example>"root", "level1", 0, "level 3"</example>
+    /// <example>"root.level1.0.level 3"</example>
+    /// </param>
+    /// <returns>false if path does not exist or the type of the destination node is not that expected</returns>
     public bool Walk<T>(params object[] path) {
-      here = root;
+      here         = root;
+      ErrorMessage = null;
       return WalkOn<T>(path);
     }
 
+    /// <summary>
+    /// Use to take a stroll down JSON lane - from where our last call to Walk or WalkOn
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <param name="path">
+    /// Path to the target node either as a list of objects or a single string with nodes separated by '.'.
+    /// Array indexes can be numbers or strings that convert to numbers.
+    /// <example>0, "level 3"</example>
+    /// <example>"0.level 3"</example>
+    /// </param>
+    /// <returns>false if path does not exist</returns>
     public bool WalkOn(params object[] path) {
       if ((path.Length == 1) && (path[0] is string)) {
         string[] split = ((string) path[0]).Split('.');
@@ -73,15 +156,34 @@ namespace Askowl {
       return true;
     }
 
+    /// <summary>
+    /// Shortcut to walk further to a node and check the node is of the type expected.
+    /// </summary>
+    /// <remarks>It will fill <see cref="ErrorMessage"/> if there is a fault.</remarks>
+    /// <param name="path">
+    /// Path to the target node either as a list of objects or a single string with nodes separated by '.'.
+    /// Array indexes can be numbers or strings that convert to numbers.
+    /// <example>0, "level 3"</example>
+    /// <example>"0.level 3"</example>
+    /// </param>
+    /// <returns>false if path does not exist</returns>
     public bool WalkOn<T>(params object[] path) { return WalkOn(path) && CheckIsA<T>(); }
 
-    public string[] List(params string[] path) {
+    /// <summary>
+    /// Returns a list of children. For a tree node, this will a list of keys.
+    /// For an array it will be a list of string representations of the elements.
+    /// On a path error it will return an empty list and set ErrorMessage
+    /// Otherwise it will return a list with one entry - being the string representation of the current node
+    /// </summary>
+    /// <param name="path">Optional absolute path to the list</param>
+    /// <returns>An array of strings as documented in the summary</returns>
+    public string[] List(params object[] path) {
       if ((path.Length > 0) && !Walk(path)) return new string[0];
 
       if (IsNode) return (new List<string>(((Node) here).Keys)).ToArray();
       if (IsArray) return Array.ConvertAll(((object[]) here), x => x.ToString());
 
-      return new string[] {here.ToString()};
+      return new[] {here.ToString()};
     }
     #endregion
     #endregion
@@ -275,6 +377,10 @@ namespace Askowl {
 
     private bool CheckIsA<T>() {
       if (IsA<T>()) return true;
+      if ((typeof(T) == typeof(int))    && IsA<long>()) return true;
+      if ((typeof(T) == typeof(float))  && IsA<double>()) return true;
+      if ((typeof(T) == typeof(float))  && IsA<long>()) return true;
+      if ((typeof(T) == typeof(double)) && IsA<long>()) return true;
 
       return AccessFailure("Expecting type {0}", typeof(T).Name);
     }
