12a9b90f342ec2801d382dca8175d9b72e58548c
paulmarrington <paul@marrington.net>
2018-06-22 10:38:21 +1000

    22/06/2018

--- Assets/Askowl/Base/Editor/CustomAssetDrawer.cs
+++ Assets/Askowl/Base/Editor/CustomAssetDrawer.cs
@@ -5,6 +5,7 @@
  */
 
 using System.Collections.Generic;
+using Askowl;
 using UnityEngine;
 using UnityEditor;
 
@@ -56,49 +57,50 @@ namespace CustomAsset {
     }
   }
   #endregion
-#region ValueLabelChange
-  [   CustomEditor(inspectedType: typeof(Base), editorForChildClasses: true)]
-  public class CustomAssetEditor : Editor { }
 
-  [CustomPropertyDrawer(type: typeof(ScriptableObject), useForChildren: true)]
-  public class ScriptableObjectDrawer : PropertyDrawer {
-    private static readonly Dictionary<string, bool>
-      FoldoutByType = new Dictionary<string, bool>();
+  #region ValueLabelChange
+  [CanEditMultipleObjects,
+   CustomEditor(inspectedType: typeof(ValueNameAttribute), editorForChildClasses: true)]
+  public class CustomAssetEditor : Editor {
+    public override void OnInspectorGUI() {
+      Debug.LogWarningFormat(
+        "**** CustomAssetDrawer:65 serializedObject={0}  #### DELETE-ME #### 21/6/18 8:04 PM",
+        serializedObject); //#DM#//
 
-    private Editor editor = null;
+      var serializedProperties = serializedObject.GetIterator();
 
+      if (serializedProperties.NextVisible(true)) {
+        do {
+          Debug.LogWarningFormat(
+            "**** CustomAssetDrawer:69 serializedProperties.name={0}  #### DELETE-ME #### 21/6/18 8:02 PM",
+            serializedProperties.name); //#DM#//
+        } while (serializedProperties.NextVisible(false));
+      }
+
+      base.OnInspectorGUI();
+    }
+  }
+
+  [CanEditMultipleObjects,
+   CustomEditor(inspectedType: typeof(SerializedObject), editorForChildClasses: true)]
+  public class SerializedObjectEditor : Editor { }
+
+  [CustomPropertyDrawer(type: typeof(ValueAttribute), useForChildren: true)]
+  public class CustomAssetLabelDrawer : PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+      var attr = property.serializedObject.targetObject.GetType()
+                         .GetCustomAttributes(typeof(ValueNameAttribute), false);
+
+      if (attr.Length > 0 && attr[0] is ValueNameAttribute) {
+        var valueName = attr[0] as ValueNameAttribute;
+        label.text = valueName.label;
+      }
+
       EditorGUI.PropertyField(position, property, label, includeChildren: true);
+    }
 
-      bool foldout = false;
-
-      if (property.objectReferenceValue != null) {
-        string objectReferenceValueType = property.objectReferenceValue.name;
-
-        bool foldoutExists =
-          FoldoutByType.TryGetValue(key: objectReferenceValueType, value: out foldout);
-
-        foldout = EditorGUI.Foldout(position: position, foldout: foldout, content: GUIContent.none);
-
-        if (foldoutExists) {
-          FoldoutByType[objectReferenceValueType] = foldout;
-        } else {
-          FoldoutByType.Add(objectReferenceValueType, foldout);
-        }
-      }
-
-      if (!foldout) return;
-
-      EditorGUI.indentLevel++;
-
-      if (!editor) {
-        Editor.CreateCachedEditor(
-          targetObject: property.objectReferenceValue, editorType: null,
-          previousEditor: ref editor);
-      }
-
-      editor.OnInspectorGUI();
-      EditorGUI.indentLevel--;
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+      return EditorGUI.GetPropertyHeight(property);
     }
   }
   #endregion
