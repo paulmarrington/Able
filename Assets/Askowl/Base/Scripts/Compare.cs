a81b845778e925459370c0fc4ce16fa8ab0a388a
paulmarrington <paul@marrington.net>
2018-06-26 17:05:59 +1000

    26/06/2018

--- Assets/Askowl/Base/Scripts/Compare.cs
+++ Assets/Askowl/Base/Scripts/Compare.cs
@@ -8,15 +8,25 @@ namespace Askowl {
     /// <summary>
     /// Check two floating point numbers to be within rounding tolerance.
     /// </summary>
-    public static bool AlmostEqual(float a, float b, float minimumChange = 1e-5f) {
+    public static bool AlmostEqual(float a, float b, float minimumChange) {
       return Math.Abs(a - b) < minimumChange;
     }
 
     /// <summary>
     /// Check two double floating point numbers to be within rounding tolerance.
     /// </summary>
-    public static bool AlmostEqual(double a, double b, double minimumChange = 1e-5) {
+    public static bool AlmostEqual(double a, double b, double minimumChange) {
       return Math.Abs(a - b) < minimumChange;
     }
+
+    /// <summary>
+    /// Check two floating point numbers to be within rounding tolerance.
+    /// </summary>
+    public static bool AlmostEqual(float a, float b) { return AlmostEqual(a, b, 1e-5f); }
+
+    /// <summary>
+    /// Check two double floating point numbers to be within rounding tolerance.
+    /// </summary>
+    public static bool AlmostEqual(double a, double b) { return AlmostEqual(a, b, 1e-5); }
   }
 }
