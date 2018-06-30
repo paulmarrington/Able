b6fc2ff606eb37c5da08d5e57ac1fb9018644eec
paulmarrington <paul@marrington.net>
2018-06-30 11:24:09 +1000

    30/06/2018

---
+++ Assets/Askowl/Base/Scripts/ExponentialMovingAverage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class ExponentialMovingAverage {
+  /// Decay parameter (0 &lt; alpha &lt; 1);
+  public double alpha = 0.2;
+
+  private bool   hasLastValue;
+  private double lastValue;
+
+  public double Average(double value) {
+    if (!hasLastValue) {
+      hasLastValue = true;
+      return lastValue = value;
+    }
+
+    double emaValue = lastValue + alpha * (value - lastValue);
+    lastValue = emaValue;
+    return emaValue;
+  }
+}
