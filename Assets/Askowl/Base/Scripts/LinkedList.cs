d2250bf39a66fe72865fbbc81dfa17ea0d8ded79
paulmarrington <paul@marrington.net>
2018-07-20 14:51:49 +1000

    20/07/2018

--- Assets/Askowl/Base/Scripts/LinkedList.cs
+++ Assets/Askowl/Base/Scripts/LinkedList.cs
@@ -6,74 +6,48 @@ using UnityEngine;
 namespace Askowl {
   public class LinkedList<T> {
     public class Node {
-      internal Node Previous, Next;
-      public   T    Item;
+      public Node          Previous, Next;
+      public LinkedList<T> owner,    lastOwner;
+      public T             Item;
+
+      public bool InRange => owner.InRange(Item);
+
+      public Node MoveTo(LinkedList<T> to) => to.Insert(this);
     }
 
-    public LinkedList(bool sorted) {
-      inRange = (t => false);
-      if (sorted) Debug.LogWarning($"Can't sort {typeof(T).Name} without a comparator");
-    }
+    public LinkedList() { InRange = (t => false); }
 
-    public LinkedList(Func<T, bool> inRangeComparator) { inRange = inRangeComparator; }
+    public LinkedList(Func<T, bool> inRangeComparator) { InRange = inRangeComparator; }
 
-    private Node          first, current;
-    private Func<T, bool> inRange;
+    public Func<T, bool> InRange;
 
-    public T First => Empty ? default(T) : (current = first).Item;
+    public Node First { get; private set; }
 
-    public T Next => ((current = current?.Next) == null) ? default(T) : current.Item;
+    public bool Empty => (First == null);
 
-    public T Previous =>
-      (current?.Previous == null) ? current.Item : (current = current.Previous).Item;
-
-    public bool InRange => (current != null) && inRange(current.Item);
-
-    public bool Empty => ((current = first) == null);
-
-    public object Mark { get { return current ?? first; } set { current = (Node) value; } }
-
-    public object Add(T newItem) => Insert(NewNode(newItem));
-
-    public void Link(object o) {
-      var node = o as Node;
-      Debug.Log($"**** LinkedList:37 To Be Done!!!"); //#DM#// 18 Jul 2018
-    }
-
-    public T Unlink() {
-      var node = current;
-      current = first;
-      Unlink(node);
-      return node.Item;
-    }
-
-    public T MoveTo(LinkedList<T> to) {
-      var node = current;
-      current = current.Next;
-      to.Unlink(node);
-      return node.Item;
-    }
+    public Node Add(T newItem) => Insert(NewNode(newItem));
 
     private Node NewNode(T item) {
-      Node node = new Node() {Item = item};
+      Node node = new Node() {Item = item, owner = this};
       return node;
     }
 
     private Node Insert(Node nodeToInsert) {
-      var next = first;
-      if (first == null) return first = nodeToInsert;
+      Unlink(nodeToInsert);
+      nodeToInsert.lastOwner = nodeToInsert.owner;
+      nodeToInsert.owner     = this;
+      if (Empty) return First = nodeToInsert;
 
-      while (inRange(next.Item)) {
+      Node next;
+
+      for (next = First; InRange(next.Item); next = next.Next) {
         if (next.Next == null) return next.Next = nodeToInsert;
-
-        next = next.Next;
       }
 
-      Unlink(nodeToInsert);
       nodeToInsert.Next     = next;
       nodeToInsert.Previous = next.Previous;
       next.Previous         = nodeToInsert;
-      if (next == first) first = nodeToInsert;
+      if (next == First) First = nodeToInsert;
       return nodeToInsert;
     }
 
@@ -81,12 +55,13 @@ namespace Askowl {
       if (node.Previous != null) {
         node.Previous.Next = node.Next;
       } else {
-        first = node.Next;
+        First = node.Next;
       }
 
       if (node.Next != null) node.Next.Previous = node.Previous;
 
       node.Previous = node.Next = null;
+      node.owner    = null;
       return node;
     }
   }
