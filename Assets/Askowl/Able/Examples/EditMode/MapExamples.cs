73b341021ef936903dcf867119bbf79e369089a3
paulmarrington <paul@marrington.net>
2018-08-26 21:46:02 +1000

    26/08/2018

--- Assets/Askowl/Able/Examples/EditMode/MapExamples.cs
+++ Assets/Askowl/Able/Examples/EditMode/MapExamples.cs
@@ -5,5 +5,87 @@ using NUnit.Framework;
 
 namespace Askowl.Examples {
   /// <a href="">Using <see cref=""/></a>
-  public class MapExamples { }
+  public class MapExamples {
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Map() {
+      var map = new Map();
+      Assert.AreEqual(0, map.Count);
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Count() {
+      var map = new Map("One", 1, "Two", 2, "Three", 3);
+
+      Assert.AreEqual(3, map.Count);
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Set() {
+      var set = new Map().Set(1, 3, 5, 7, 9);
+
+      Assert.AreEqual(5, set.Count);
+
+      set.Set(11, 13, 15);
+
+      Assert.AreEqual(8, set.Count);
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Add() {
+      var map = new Map("One", 1, "Two", 2, "Three", 3);
+
+      map.Add("Four", 4, "Five", 5);
+
+      Assert.AreEqual(5, map.Count);
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Contains() {
+      var map = new Map("One", 1, "Two", 2, "Three", 3, "Four", 4, "Five", 5);
+
+      Assert.IsTrue(map.Contains("One"));
+      Assert.IsTrue(map.Contains("Three"));
+      Assert.IsTrue(map.Contains("Five"));
+      Assert.IsFalse(map.Contains("Seven"));
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void IsA() {
+      var map = new Map("One", 1, "Two", "2", "Three", new Map());
+
+      Assert.IsTrue(map.IsA<int>("One"));
+      Assert.IsTrue(map.IsA<string>("Two"));
+      Assert.IsTrue(map.IsA<Map>("Three"));
+      Assert.IsFalse(map.IsA<int>("Two"));
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Get() {
+      var map = new Map("One", 111, "Two", "2", "Three", new Map());
+
+      Assert.AreEqual(111,  map.Get<int>("One"));
+      Assert.AreEqual("2",  map.Get<string>("Two"));
+      Assert.AreEqual(0,    map.Get<Map>("Three").Count);
+      Assert.AreEqual(null, map.Get<int>("Two"));
+    }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Closest() { }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Key() { }
+
+    /// <a href="">Using <see cref=""/></a>
+    [Test]
+    public void Value() { }
+  }
 }
--- Assets/Askowl/Able/Scripts/Memory/Map.cs
+++ Assets/Askowl/Able/Scripts/Memory/Map.cs
@@ -1,28 +1,93 @@
 ﻿// Copyright 2018 (C) paul@marrington.net http://www.askowl.net/unity-packages
 
 using System;
-using System.Text;
 
 namespace Askowl {
+  /// <a href=""></a>
   public class Map {
-    public struct Node {
-      private object key, value;
-    }
+    private object[] keys, values;
 
-    private Node[] nodes;
-    private int    count;
+    /// <a href=""></a>
+    public int Count { get; private set; }
 
-    private void Add(object key, object value) {
-      if (count)
-    }
-
-    public static Map Set(params object[] keys) { }
-
+    /// <a href=""></a>
     public Map(params object[] keyValuePairs) {
-      count = keyValuePairs.Length / 2;
-      nodes = new Node[(count < 4) ? 4 : count];
+      var length = (Count < 4) ? 4 : Count;
+      keys   = new object[length];
+      values = new object[length];
+      Add(keyValuePairs);
+    }
 
-      for (int i = 0; i < keyValuePairs.Length; i += 2) { }
+    /// <a href=""></a>
+    public Map Set(params object[] newKeys) {
+      var oldCount = Count;
+      Count += keys.Length;
+
+      if (keys.Length < Count) Resize(Count);
+
+      for (int i = oldCount, j = 0; i < Count; i++) {
+        keys[i]   = keys[j++];
+        values[i] = null;
+      }
+
+      return this;
+    }
+
+    /// <a href=""></a>
+    public Map Add(params object[] keyValuePairs) {
+      var oldCount = Count;
+      Count += keyValuePairs.Length / 2;
+
+      if (keys.Length < Count) Resize(Count);
+
+      for (int i = oldCount, j = 0; i < Count; i++) {
+        keys[i]   = keyValuePairs[j++];
+        values[i] = keyValuePairs[j++];
+      }
+
+      return this;
+    }
+
+    /// <a href=""></a>
+    public bool Contains(object key) {
+      Closest = Array.BinarySearch(array: keys, value: Key = key);
+
+      if (Closest >= 0) {
+        Value = values[Closest];
+        return true;
+      }
+
+      Closest = ~Closest;
+      Value   = null;
+      return false;
+    }
+
+    /// <a href=""></a>
+    public bool IsA<T>(object key) => Contains(key) && (Value is T);
+
+    /// <a href=""></a>
+    public T Get<T>(object key) where T : class => Contains(key) ? (Value as T) : null;
+
+    /// <a href=""></a>
+    public int Closest { get; private set; }
+
+    /// <a href=""></a>
+    public object Key { get; private set; }
+
+    /// <a href=""></a>
+    public object Value { get; private set; }
+
+    private void Resize(int to) {
+      var length              = keys.Length * 2;
+      if (length < to) length = to;
+      keys   = Resize(from: keys,   length: length);
+      values = Resize(from: values, length: length);
+    }
+
+    private object[] Resize(object[] from, int length) {
+      var larger = new object[length];
+      Array.Copy(sourceArray: from, destinationArray: larger, length: from.Length);
+      return larger;
     }
   }
 }
