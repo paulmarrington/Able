d82f559544536a29b723bbdba49a9e74b79f92ee
paulmarrington <paul@marrington.net>
2019-02-26 11:05:59 +1000

    prepare fibers 2.1 release

--- Assets/Askowl/Able/Examples/Introduction.cs
+++ Assets/Askowl/Able/Examples/Introduction.cs
@@ -4,11 +4,19 @@ using System;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Askowl.Fibers.Examples {
   /// Servicing "Introduction to Fibers" scene
   public class Introduction : MonoBehaviour {
+    [SerializeField] private string homePageUrl      = "http://www.askowl.net/";
+    [SerializeField] private string documentationUrl = "https://paulmarrington.github.io/Unity-Documentation/";
+    [SerializeField] private string doxygenUrl =
+      "https://paulmarrington.github.io/Unity-Documentation/xxxx/Doxygen/html/annotated.html";
+    [SerializeField] private string videosUrl      = "https://www.youtube.com/playlist?list=";
+    [SerializeField] private string supportUrl     = "https://www.patreon.com/paulmarrington";
+    [SerializeField] private string exampleScene   = default;
     [SerializeField] private Toggle testsAvailable = default;
 
     private bool inStart;
@@ -27,27 +35,31 @@ namespace Askowl.Fibers.Examples {
     public void EnableTests() {
       if (inStart) return;
       var value = testsAvailable.isOn ? 1 : 0;
-      File.WriteAllText("Assets/Askowl/Askowl.json", $"{{\"EnableTesting\": {value}\n\"Update\": \"{DateTime.Now}\"}}");
+      File.WriteAllText(
+        "Assets/Askowl/Able/Configuration.json", $"{{\"EnableTesting\": {value}\n\"Update\": \"{DateTime.Now}\"}}");
       EditorApplication.isPlaying = false;
     }
 
     /// When "Home Page" button is pressed
-    public void HomePageButton() => Application.OpenURL("http://www.askowl.net/unity-fibers");
+    public void HomePageButton() => Application.OpenURL(homePageUrl);
 
     /// When "Home Page" button is pressed
-    public void DocumentationButton() =>
-      Application.OpenURL("https://paulmarrington.github.io/Unity-Documentation/Fibers/");
+    public void DocumentationButton() => Application.OpenURL(documentationUrl);
 
     /// When "Home Page" button is pressed
-    public void VideosButton() =>
-      Application.OpenURL("https://www.youtube.com/playlist?list=PLufDQjKXLJJpT2Vkz6LJPyrBkm8HNL4UP");
+    public void VideosButton() => Application.OpenURL(videosUrl);
 
     /// When "Home Page" button is pressed
-    public void DoxygenButton() => Application.OpenURL(
-      "https://paulmarrington.github.io/Unity-Documentation/Fibers/Doxygen/html/annotated.html");
+    public void DoxygenButton() => Application.OpenURL(doxygenUrl);
 
     /// When "Home Page" button is pressed
-    public void SupportButton() => Application.OpenURL("https://www.patreon.com/paulmarrington");
+    public void SupportButton() => Application.OpenURL(supportUrl);
+
+    /// When "Example Scene" button is pressed
+    public void ExampleSceneButton() {
+      if (!Application.CanStreamedLevelBeLoaded(exampleScene)) PlayModeController.AddSceneToBuildSettings(exampleScene);
+      SceneManager.LoadScene(exampleScene);
+    }
   }
 
   /// <inheritdoc />
@@ -55,7 +67,7 @@ namespace Askowl.Fibers.Examples {
     static AskowlTest() => EditorApplication.playModeStateChanged += OnPlayModeState;
     private static void OnPlayModeState(PlayModeStateChange state) {
       if (state == PlayModeStateChange.EnteredEditMode) {
-        using (var json = Json.Instance.Parse(File.ReadAllText("Assets/Askowl/Askowl.json"))) {
+        using (var json = Json.Instance.Parse(File.ReadAllText("Assets/Askowl/Able/Configuration.json"))) {
           var enableTests = json.Node.To("EnableTesting").Found && (json.Node.Long == 1);
           AddOrRemoveDefines(enableTests, "AskowlTests");
         }
